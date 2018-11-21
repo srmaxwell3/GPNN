27b3bdc5566a52d802f3334af7c2fd067754dd7f
Sid Maxwell <srmaxwell@gmail.com>
2018-11-21 18:52:02 +0000

    Changes...

--- gp.cc
+++ gp.cc
@@ -55,7 +55,8 @@ public:
   {
   }
   GKind getKind() const { return kind; }
-  GNode *getNext() const { return lChild; }
+  GNode *getNext() const { if (!isDone()) { return lChild; } return 0; }
+  GNode *getSibling() const { return rChild; }
   bool isDone() const { return kind == End; }
   bool hasMore() const { return !isDone(); }
   string toString() const {
@@ -88,107 +89,19 @@ GNode *GenWDec(GNode *lChild) { return new GNode(WDec, lChild); }
 GNode *GenPar(GNode *lChild, GNode *rChild) { return new GNode(Par, lChild, rChild); }
 GNode *GenSer(GNode *lChild, GNode *rChild) { return new GNode(Ser, lChild, rChild); }
 
-class InputNode;
-class OutputNode;
-
-class InputNode : public vector<OutputNode *> {
-public:
-  InputNode(std::initializer_list<OutputNode *> oNodes) :
-    vector<OutputNode *>(oNodes)
-  {
-  }
-  InputNode(vector<OutputNode *> const &oNodes) :
-    vector<OutputNode *>(oNodes)
-  {
-  }
-  InputNode() { }
-  virtual ~InputNode() { }
-  virtual string Name() const { return "InputNode"; }
-  virtual string toString() const = 0;
-  void addOutputTo(OutputNode *oNode) {
-    push_back(oNode);
-  }
-  void removeOutputTo(OutputNode *oNode) {
-    auto o = find(begin(), end(), oNode);
-    if (o != end()) {
-      erase(o);
-    }
-  }
-};
-
-struct Input {
-  Input(InputNode *_iNode, int _weight = 1) : iNode(_iNode), weight(_weight) { }
-  bool operator==(Input const &that) const { return iNode == that.iNode; }
-  bool operator==(InputNode const *&that) const { return iNode == that; }
-  bool operator<(Input const &that) const { return iNode < that.iNode || (iNode == that.iNode && weight < that.weight); }
-  bool operator<(InputNode const *&that) const { return iNode < that; }
-  void addOutputTo(OutputNode *oNode) { iNode->addOutputTo(oNode); }
-  void removeOutputTo(OutputNode *oNode) { iNode->removeOutputTo(oNode); }
-  string Name() const { return iNode->Name(); }
-  string toString() const { return iNode->toString(); }
-  
-  InputNode *iNode;
-  int weight;
-};
-
-class OutputNode : public vector<Input> {
-public:
-  OutputNode(std::initializer_list<InputNode *> iNodes, int iLink) :
-    inputLink(iLink)
-  {
-    for (auto i = iNodes.begin(); i != iNodes.end(); i += 1) {
-      push_back(Input(*i));
-    }
-  }
-  OutputNode(vector<InputNode *> const &iNodes, int iLink) :
-    inputLink(iLink)
-  {
-    for (auto i = iNodes.begin(); i != iNodes.end(); i += 1) {
-      push_back(Input(*i));
-    }
-  }
-  OutputNode(std::initializer_list<Input> iNodes, int iLink) :
-    vector<Input>(iNodes),
-    inputLink(iLink)
-  {
-  }
-  OutputNode() : inputLink(0) { }
-  virtual ~OutputNode() { }
-  virtual string Name() const { return "OutputNode"; }
-  virtual string toString() const = 0;
-  void addInputFrom(InputNode *iNode) {
-    push_back(iNode);
-  }
-  void removeInputFrom(InputNode *iNode) {
-    auto i = find(begin(), end(), iNode);
-    if (i != end()) {
-      erase(i);
-    }
-  }
-  size_t inputLinkAsIndex() const {
-    if (inputLink < 0) {
-      return (inputLink % size()) + size();
-    }
-    return 0 < size() ? inputLink % size() : 0;
-  }
-
-protected:
-  int inputLink;
-};
-
 class INode;
 class ONode;
 class PNode;
 
-vector<InputNode *> INodes;
-vector<OutputNode *> ONodes;
+vector<INode *> INodes;
+vector<ONode *> ONodes;
 vector<PNode *> PNodes;
 
-class INode : public InputNode {
+class INode : public vector<ONode *> {
 public:
-  INode(std::initializer_list<OutputNode *> oNodes);
-  INode(vector<OutputNode *> const &oNodes);
-  INode() : InputNode() { }
+  INode(std::initializer_list<ONode *> oNodes);
+  INode(vector<ONode *> const &oNodes);
+  INode() { }
   virtual string Name() const {
     ostringstream s;
     auto i = find(INodes.cbegin(), INodes.cend(), this);
@@ -199,15 +112,39 @@ public:
     }
     return s.str();
   }
-  string toString() const;
+  virtual string toString() const;
+  void addOutputTo(ONode *oNode) {
+    push_back(oNode);
+  }
+  void removeOutputTo(ONode *oNode) {
+    auto o = find(begin(), end(), oNode);
+    if (o != end()) {
+      erase(o);
+    }
+  }
 };
 
-class ONode : public OutputNode {
+struct Input {
+  Input(INode *_iNode, int _weight = 1) : iNode(_iNode), weight(_weight) { }
+  bool operator==(Input const &that) const { return iNode == that.iNode; }
+  bool operator==(INode const *&that) const { return iNode == that; }
+  bool operator<(Input const &that) const { return iNode < that.iNode || (iNode == that.iNode && weight < that.weight); }
+  bool operator<(INode const *&that) const { return iNode < that; }
+  void addOutputTo(ONode *oNode) { iNode->addOutputTo(oNode); }
+  void removeOutputTo(ONode *oNode) { iNode->removeOutputTo(oNode); }
+  string Name() const { return iNode->Name(); }
+  string toString() const { return iNode->toString(); }
+  
+  INode *iNode;
+  int weight;
+};
+
+class ONode : public vector<Input> {
 public:
-  ONode(std::initializer_list<InputNode *> iNodes, int iLink = 0);
-  ONode(vector<InputNode *> const &iNodes, int iLink = 0);
-  ONode(std::initializer_list<Input> iNodes, int iLink = 0);
-  ONode() : OutputNode() { }
+  ONode(std::initializer_list<INode *> iNodes, int _iLink = 0);
+  ONode(vector<INode *> const &iNodes, int _iLink = 0);
+  ONode(std::initializer_list<Input> iNodes, int _iLink = 0);
+  ONode() : iLink(0) { }
   virtual string Name() const {
     ostringstream s;
     auto i = find(ONodes.cbegin(), ONodes.cend(), this);
@@ -218,48 +155,77 @@ public:
     }
     return s.str();
   }
-  string toString() const;
+  virtual string toString() const;
+  void addInputFrom(INode *iNode) {
+    push_back(iNode);
+  }
+  void removeInputFrom(INode *iNode) {
+    auto i = find(begin(), end(), iNode);
+    if (i != end()) {
+      erase(i);
+    }
+  }
+  size_t iLinkAsIndex() const {
+    if (iLink < 0) {
+      return (iLink % size()) + size();
+    }
+    return 0 < size() ? iLink % size() : 0;
+  }
+  void bumpInputWeight(int by) {
+    Input &input = (*this)[iLinkAsIndex()];
+    input.weight += by;
+  }
+  void cutINode() {
+    size_t i = iLinkAsIndex();
+    Input &input = (*this)[i];
+    INode *iNode = input.iNode;
+    iNode->removeOutputTo(this);
+    erase(begin() + i);
+  }
+
+protected:
+  int iLink;
 };
 
-class PNode : public InputNode, public OutputNode {
+class PNode : public INode, public ONode {
 public:
-  PNode(std::initializer_list<OutputNode *> oNodes,
-	std::initializer_list<InputNode *> iNodes,
-	int iLink = 0,
+  PNode(std::initializer_list<ONode *> oNodes,
+	std::initializer_list<INode *> iNodes,
+	int _iLink = 0,
 	int _threshold = 0,
 	GNode *_genome = 0,
 	GNode *_genomeReader = 0
        ) :
-    InputNode(oNodes),
-    OutputNode(iNodes, iLink),
+    INode(oNodes),
+    ONode(iNodes, _iLink),
     threshold(_threshold),
     genome(_genome),
     genomeReader(_genomeReader)
   {
   }
-  PNode(vector<OutputNode *> const &oNodes,
-	vector<InputNode *> const &iNodes,
-	int iLink = 0,
+  PNode(vector<ONode *> const &oNodes,
+	vector<INode *> const &iNodes,
+	int _iLink = 0,
 	int _threshold = 0,
 	GNode *_genome = 0,
 	GNode *_genomeReader = 0
        ) :
-    InputNode(oNodes),
-    OutputNode(iNodes, iLink),
+    INode(oNodes),
+    ONode(iNodes, _iLink),
     threshold(_threshold),
     genome(_genome),
     genomeReader(_genomeReader)
   {
   }
-  PNode(std::initializer_list<OutputNode *> oNodes,
+  PNode(std::initializer_list<ONode *> oNodes,
 	std::initializer_list<Input> iNodes,
-	int iLink = 0,
+	int _iLink = 0,
 	int _threshold = 0,
 	GNode *_genome = 0,
 	GNode *_genomeReader = 0
        ) :
-    InputNode(oNodes),
-    OutputNode(iNodes, iLink),
+    INode(oNodes),
+    ONode(iNodes, _iLink),
     threshold(_threshold),
     genome(_genome),
     genomeReader(_genomeReader)
@@ -277,67 +243,146 @@ public:
   }
   string toString() const {
     ostringstream s;
-    s << Name() << ": {";
+    s << Name() << ": { { outputNodes = {";
+    for (auto n = INode::cbegin(); n != INode::cend(); n++) {
+      if (n != INode::cbegin()) {
+	s << ",";
+      }
+      s << " " << (*n)->Name();
+    }
+    s << " } }, { inputNodes = {";
+    int i = iLinkAsIndex();
+    for (auto n = ONode::cbegin(); n != ONode::cend(); n++) {
+      if (n != ONode::cbegin()) {
+	s << ",";
+      }
+      if (i-- == 0) {
+	s << " <" << n->Name() << "(" << n->weight << ")>";
+      } else {
+	s << " " << n->Name() << "(" << n->weight << ")";
+      }
+    }
+    s << " }";
+    s << ", iLink = " << iLink << " }";
+    s << ", threshold = " << threshold;
+    s << ", genomeReader = " << genomeReader->toString();
     s << " }";
     return s.str();
   }
   void addInputFrom(Input iNode) {
-    OutputNode::push_back(iNode);
+    ONode::push_back(iNode);
     iNode.addOutputTo(this);
   }
-  void addOutputTo(OutputNode *oNode) {
-    InputNode::push_back(oNode);
+  void addOutputTo(ONode *oNode) {
+    INode::push_back(oNode);
     oNode->addInputFrom(this);
   }
+  GKind getKind() const { return genomeReader->getKind(); }
   bool Grow() {
     switch (genomeReader->getKind()) {
     case Ser:
       splitSerially();
+      genomeReader = genomeReader->getNext();
       break;
     case Par:
       splitParallel();
+      genomeReader = genomeReader->getNext();
       break;
     case LInc:
       bumpInputLink(+1);
+      genomeReader = genomeReader->getNext();
       break;
     case LDec:
       bumpInputLink(-1);
+      genomeReader = genomeReader->getNext();
       break;
     case WInc:
       bumpInputWeight(+1);
+      genomeReader = genomeReader->getNext();
       break;
     case WDec:
       bumpInputWeight(-1);
+      genomeReader = genomeReader->getNext();
       break;
     case TInc:
       bumpThreshold(+1);
+      genomeReader = genomeReader->getNext();
       break;
     case TDec:
       bumpThreshold(-1);
+      genomeReader = genomeReader->getNext();
       break;
     case Cut:
-      cutInputNode();
+      cutINode();
+      genomeReader = genomeReader->getNext();
       break;
     case Wait:
       wait();
+      genomeReader = genomeReader->getNext();
       break;
     case End:
       done();
       break;
     }
+
     return isDone();
   }
   void splitSerially() {
+    PNode *sibling = new PNode();
+    PNodes.push_back(sibling);
+
+    // We'll keep our inputs, our sibling will get our outputs, and we
+    // and our sibling will be connected, via our output to its input.
+
+    for (auto n = INode::begin(); n != INode::end(); n++) {
+      sibling->addOutputTo(*n);
+      (*n)->removeInputFrom(this);
+    }
+    INode::clear();
+
+    addOutputTo(sibling);
+    sibling->iLink = 0;
+    sibling->threshold = threshold;
+    sibling->genome = genome;
+    sibling->genomeReader = genomeReader->getSibling();
+
+    // cout << "PNode::cloneSerially():\n";
+    // cout << "    this->" << toString() << "\n";
+    // cout << "    sibling->" << toString() << "\n";
   }
   void splitParallel() {
+    PNode *sibling = new PNode();
+    PNodes.push_back(sibling);
+
+    // We'll keep our inputs and outputs, our sibling will get a copy
+    // of both.
+
+    for (auto n = INode::cbegin(); n != INode::cend(); n++) {
+      sibling->addOutputTo(*n);
+    }
+    for (auto n = ONode::cbegin(); n != ONode::cend(); n++) {
+      sibling->addInputFrom(*n);
+    }
+    sibling->iLink = iLink;
+    sibling->threshold = threshold;
+    sibling->genome = genome;
+    sibling->genomeReader = genomeReader->getSibling();
+
+    // cout << "PNode::cloneParallelly():\n";
+    // cout << "    this->" << toString() << "\n";
+    // cout << "    sibling->" << toString() << "\n";
   }
   void bumpInputLink(int by) {
+    iLink += by;
   }
   void bumpInputWeight(int by) {
+    ONode::bumpInputWeight(by);
   }
   void bumpThreshold(int by) {
+    threshold += by;
   }
-  void cutInputNode() {
+  void cutINode() {
+    ONode::cutINode();
   }
   void wait() {
   }
@@ -351,19 +396,23 @@ public:
   }
 
 private:
+  PNode() : threshold(0), genome(0), genomeReader(0) { }
+
   int threshold;
   GNode *genome;
   GNode *genomeReader;
 };
 
-INode::INode(std::initializer_list<OutputNode *> oNodes) : InputNode(oNodes) {
-  for (auto o = begin(); o != end(); o++) {
+INode::INode(std::initializer_list<ONode *> oNodes) {
+  for (auto o = oNodes.begin(); o != oNodes.end(); o++) {
+    push_back(*o);
     (*o)->addInputFrom(this);
   }
 }
 
-INode::INode(vector<OutputNode *> const &oNodes) : InputNode(oNodes) {
-  for (auto o = begin(); o != end(); o++) {
+INode::INode(vector<ONode *> const &oNodes) {
+  for (auto o = oNodes.begin(); o != oNodes.end(); o++) {
+    push_back(*o);
     (*o)->addInputFrom(this);
   }
 }
@@ -372,78 +421,61 @@ string INode::toString() const {
   ostringstream s;
   s << Name() << ": { outputNodes = {";
   for (auto n = cbegin(); n != cend(); n++) {
-    s << " " << (*n)->Name();
-
     if (n != cbegin()) {
       s << ",";
     }
+    s << " " << (*n)->Name();
   }
   s << " } }";
   return s.str();
 }
 
-ONode::ONode(std::initializer_list<InputNode *> iNodes, int iLink) :
-  OutputNode(iNodes, iLink)
+ONode::ONode(std::initializer_list<INode *> iNodes, int _iLink) :
+  iLink(_iLink)
 {
-  for (auto i = begin(); i != end(); i++) {
-    i->addOutputTo(this);
+  for (auto i = iNodes.begin(); i != iNodes.end(); i++) {
+    push_back({ *i, 1 });
+    (*i)->addOutputTo(this);
   }
 }
 
-ONode::ONode(vector<InputNode *> const &iNodes, int iLink) :
-  OutputNode(iNodes, iLink)
+ONode::ONode(vector<INode *> const &iNodes, int _iLink) :
+  iLink(_iLink)
 {
-  for (auto i = begin(); i != end(); i++) {
-    i->addOutputTo(this);
+  for (auto i = iNodes.begin(); i != iNodes.end(); i++) {
+    push_back({ *i, 1 });
+    (*i)->addOutputTo(this);
   }
 }
 
-ONode::ONode(std::initializer_list<Input> iNodes, int iLink) :
-  OutputNode(iNodes, iLink)
+ONode::ONode(std::initializer_list<Input> iNodes, int _iLink) :
+  iLink(_iLink)
 {
-  for (auto i = begin(); i != end(); i++) {
-    i->addOutputTo(this);
+  for (auto i = iNodes.begin(); i != iNodes.end(); i++) {
+    push_back(*i);
+    i->iNode->addOutputTo(this);
   }
 }
 
 string ONode::toString() const {
   ostringstream s;
   s << Name() << ": { inputNodes = {";
+  int i = iLinkAsIndex();
   for (auto n = cbegin(); n != cend(); n++) {
-    s << " " << n->Name();
-
     if (n != cbegin()) {
       s << ",";
     }
+    if (i-- == 0) {
+      s << " <" << n->Name() << "(" << n->weight << ")>";
+    } else {
+      s << " " << n->Name() << "(" << n->weight << ")";
+    }
   }
-  s << " }, inputLink = " << inputLink << " }";
+  s << " }, iLink = " << iLink << " }";
   return s.str();
 }
 
-int main(int argc, char const *argv[])
-{
-  GNode *genome =
-    GenSer(GenPar(GenEnd(),
-		  GenEnd()
-		 ),
-	   GenSer(GenPar(GenTInc(GenEnd()),
-			 GenEnd()
-			),
-		  GenWDec(GenEnd())
-		 )
-	   );
-
-  cout << "genome = " << genome->toString() << "\n";
-
-  ONode *oNode = new ONode();
-  ONodes.push_back(oNode);
-
-  INode *iNode = new INode();
-  INodes.push_back(iNode);
-
-  PNode *pNode = new PNode(ONodes, INodes, 0, 0, genome, genome);
-  PNodes.push_back(pNode);
-
+void Dump() {
   cout << "Outputs: {\n";
   for (auto o = ONodes.cbegin(); o != ONodes.cend(); o++) {
     cout << "    " << (*o)->toString() << "\n";
@@ -456,6 +488,70 @@ int main(int argc, char const *argv[])
   }
   cout << "}\n";
 
-  cout << "Ancestor: " << pNode->toString() << "\n";
+  cout << "Programs: {\n";
+  for (auto p = PNodes.cbegin(); p != PNodes.cend(); p++) {
+    cout << "    " << (*p)->toString() << "\n";
+  }
+  cout << "}\n";
+}
+
+int main(int argc, char const *argv[])
+{
+  GNode *genome =
+    GenSer(GenPar(GenLInc(GenCut(GenEnd())),
+		  GenCut(GenEnd())
+		 ),
+	   GenSer(GenPar(GenTInc(GenEnd()),
+			 GenEnd()
+			),
+		  GenWDec(GenEnd())
+		 )
+	   );
+
+  cout << "genome = " << genome->toString() << "\n";
+
+  // ONode *oNode = new ONode();
+  // ONodes.push_back(oNode);
+  // 
+  // INode *iNode = new INode();
+  // INodes.push_back(iNode);
+  // 
+  // PNode *pNode = new PNode(ONodes, INodes, 0, 0, genome, genome);
+  // PNodes.push_back(pNode);
+
+  ONodes.push_back(new ONode());
+  ONodes.push_back(new ONode());
+
+  INodes.push_back(new INode());
+  INodes.push_back(new INode());
+  INodes.push_back(new INode());
+
+  PNodes.push_back(new PNode(ONodes, INodes, 0, 0, genome, genome));
+  
+  Dump();
+
+  bool isDone = false;
+  for (size_t cycle = 0; !isDone; cycle += 1) {
+    size_t hasMore = 0;
+    size_t nPNodes = PNodes.size();
+
+    for (size_t p = 0; p < nPNodes; p += 1) {
+      PNode *pNode = PNodes[p];
+
+      if (pNode->hasMore()) {
+	cout << "# Growing by " << ::toString(pNode->getKind()) << ": " << pNode->toString() << "\n";
+
+	if (!pNode->Grow()) {
+	  hasMore += 1;
+	}
+      }
+    }
+
+    cout << "# " << cycle << " ------------------------------------------------------------------------\n";
+    Dump();
+
+    isDone = 0 == hasMore && nPNodes == PNodes.size();
+  }
+
   return 0;
 }
